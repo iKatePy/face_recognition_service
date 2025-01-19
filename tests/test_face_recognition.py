0faf3c7bfb3e0c958b9e5940ed0f2e7f4e71e4c8
iKatePy <107533058+iKatePy@users.noreply.github.com>
2025-01-19 20:45:22 +0500

    Update test_face_recognition.py

--- tests/test_face_recognition.py
+++ tests/test_face_recognition.py
@@ -5,11 +5,11 @@ from app.face_recognition import detect_faces
 
 def test_detect_faces_valid_image():
     # Load a valid image into memory
-    with open("tests/1.jpg", "rb") as f:
+    with open("tests/Source.jpg", "rb") as f:
         image_data = f.read()
     
     # Create a mock UploadFile instance
-    upload_file = UploadFile(filename="1.jpg", file=BytesIO(image_data))
+    upload_file = UploadFile(filename="Source.jpg", file=BytesIO(image_data))
     
     result = detect_faces(upload_file)
     
